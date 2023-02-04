2c5aeacc3d086c360b810ffb191ea7ffd0ebd381
Murphy Justian <justian@umich.edu>
2023-02-04 14:35:39 -0500

    Root beer and fuel

---
+++ Assets/FuelBar.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelBar : MonoBehaviour
+{
+	private static float fuel = 100.0f;
+	public RectTransform rectTransform;
+	public static float getFuel()
+	{
+		return fuel;
+	}
+	
+	public static void depleteFuel(float amount)
+	{
+		fuel -= amount;
+	}
+	
+	public static void replenishFuel(float amount)
+	{
+		fuel += amount;
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float width = 480;
+		float height = 80;
+		
+		width *= fuel/100.0f;
+		rectTransform.sizeDelta = new Vector2(width, height);
+		if(fuel > 100.0f)
+		{
+			fuel = 100.0f;
+		}
+		
+		if(fuel < 0.0f)
+		{
+			fuel = 0.0f;
+		}
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -40,10 +40,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        doBasicMovement();
+		if(FuelBar.getFuel() > 0)
+		{
+			doBasicMovement();
 
 
-        doBurstMovement();
+			doBurstMovement();
+		}
 
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -63,21 +66,25 @@ public class PlayerController : MonoBehaviour
 
     void doRotation()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(new Vector3(0, 0, 1), rotationSpeed * Time.deltaTime);
-        }
+		if(FuelBar.getFuel() > 0)
+		{
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				transform.Rotate(new Vector3(0, 0, 1), rotationSpeed * Time.deltaTime);
+			}
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(new Vector3(0, 0, 1), -rotationSpeed * Time.deltaTime);
-        }
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				transform.Rotate(new Vector3(0, 0, 1), -rotationSpeed * Time.deltaTime);
+			}
+		}
     }
     void doBasicMovement()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(transform.up * speed);
+			FuelBar.depleteFuel(0.01f);
             Exhaust.enabled = true;
 
         }
@@ -107,6 +114,7 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("Double Tapped U");
                 rb.AddForce(transform.up * speed * burstMultiplier);
                 backBoost.Boost();
+				FuelBar.depleteFuel(5f);
             }
             else
             {
